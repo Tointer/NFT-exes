6524b5ac70b3dd683d0fe423b15c788cd3d761f0
ibragimov.i <Tointer@yandex.ru>
2022-06-11 03:04:19 +0300

    Add card for each NFT with image and name

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,6 +4,10 @@ import classnames, {
   display,
   flexDirection,
 } from 'classnames/tailwind'
+import NFTCard from 'components/landing/NFTCard'
+import { ZDK } from '@zoralabs/zdk'
+import { useState } from 'react'
+import { TokenQuery, TokensQuery } from '@zoralabs/zdk/dist/queries/queries-sdk'
 
 const pageBox = classnames(
   display('flex'),
@@ -12,9 +16,36 @@ const pageBox = classnames(
 )
 
 export default function () {
+  async function handleSubmit(e: string) {
+    const testAddress = '0xb23C6962c431524c154b3eA088fC3Ca0Dc4b0B94'
+    const zdk = new ZDK()
+
+    const args = {
+      where: {
+        ownerAddresses: [testAddress],
+      },
+    }
+    const response = await zdk.tokens(args)
+    response.tokens.nodes[0].token.image
+    console.log(response)
+    setTokens(response)
+  }
+
+  const [tokens, setTokens] = useState<TokensQuery | null>(null)
+
   return (
     <div className={pageBox}>
-      <InitialCard />
+      <InitialCard handleSubmit={handleSubmit} />
+      <div>
+        {tokens?.tokens.nodes.map((t) => {
+          return (
+            <NFTCard
+              image={t.token.image?.url ?? ''}
+              name={t.token.name ?? '...'}
+            />
+          )
+        })}
+      </div>
     </div>
   )
 }
