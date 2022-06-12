657d7a926a503da0a20f151d3e7a23a516b18362
ibragimov.i <Tointer@yandex.ru>
2022-06-12 18:30:50 +0300

    add exes to cards

--- src/components/landing/NFTCard.tsx
+++ src/components/landing/NFTCard.tsx
@@ -2,12 +2,17 @@ import { AccentText, BodyText, HeaderText } from 'components/Text'
 import { space } from 'classnames/tailwind'
 import Card from 'components/Card'
 import GetStartedButton from 'components/GetStartedButton'
+import { ethers } from 'ethers'
+import { ENSName, AddressDisplayEnum } from 'react-ens-name'
 
 const initialCardWrapper = space('space-y-4')
-
+const provider = new ethers.providers.JsonRpcProvider(
+  'https://mainnet.infura.io/v3/118f8087dcea46caa83d120ab84670f1'
+)
 interface NFTProps {
   image: string
   name: string
+  exes: Map<string, string>
 }
 
 export default function (props: NFTProps) {
@@ -16,6 +21,23 @@ export default function (props: NFTProps) {
       <div className={initialCardWrapper}>
         <img src={props.image} />
         <HeaderText extraLeading>{props.name}</HeaderText>
+      </div>
+      <div>
+        {Array.from(props.exes).map(([key, value]) => {
+          return (
+            <p>
+              <a href={'https://etherscan.io/address/' + key} target="_blank">
+                <ENSName
+                  address={key}
+                  provider={provider}
+                  displayType={AddressDisplayEnum.FIRST4_LAST4}
+                  withEllipses
+                ></ENSName>
+              </a>
+              : {value}
+            </p>
+          )
+        })}
       </div>
     </Card>
   )
